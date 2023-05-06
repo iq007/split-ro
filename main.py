667cdf8de6bd94e18559543f22dd75abe7fa1c44
Stefan Iancu <stefan.iancu@nuvei.com>
2023-05-06 09:02:15 +0300

    Updated main to split a RO/CUI

--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
+from split import split as split
+def print_hi(message):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {message}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Let''s split some ROs!')
+    my_ro = input('Give me RO: ')
+    a, b = split(my_ro)
+    print(f'RO: {a}')
+    print(f'CUI: {b}')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
